09458d2882f7ed0ae81b50851ceaba8e57447a1f
BoazKarniHarel <boazkarni@gmail.com>
2025-08-04 09:51:20 +0300

    feat: basic app done

---
+++ projects/sentiment_analysis/sentiment_analysis.py
@@ -0,0 +1,12 @@
+import json
+
+import requests
+
+def sentiment_analyzer(text_to_analyse: str) -> dict[str, str|float]:
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL of the sentiment analysis service
+    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
+    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+    formatted_response = json.loads(response.text)
+    response_dict = {"label": formatted_response['documentSentiment']['label'], "score": formatted_response['documentSentiment']['score']}
+    return response_dict
